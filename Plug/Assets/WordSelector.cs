6736e4ddff08b605eae1315a8019a0ab4e6eb8ba
Nyarlygames <nyarlybox@gmail.com>
2017-10-17 15:03:06 +0200

    keyboard actions on main menu

--- Plug/Assets/WordSelector.cs
+++ Plug/Assets/WordSelector.cs
@@ -1,128 +1,203 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WordSelector : MonoBehaviour {
-
-    public bool loop;
+    
     public float frameSeconds = 0.5f;
-    private SpriteRenderer spr;
-    private List<Sprite> sprites;
+    public SpriteRenderer spr;
+    public List<Sprite> sprites;
     private int frame = 0;
     private int reverser = 1;
     private float deltaTime = 0;
-    private float lastdelta = 0;
     public string word = "";
+    public List<KeyCode> letterstring = new List<KeyCode>();
     private List<Sprite> letters = new List<Sprite>();
+    private List<String> wordstring = new List<String>();
     private List<GameObject> sprite_word = new List<GameObject>();
 
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
+        gameObject.GetComponent<Transform>().localScale = new Vector3(0.5f, 0.5f, 0);
         sprites = new List<Sprite>(Resources.LoadAll<Sprite>("img/cursor/"));
-        letters.Add(Resources.Load<Sprite>("img/letters/P"));
-        letters.Add(Resources.Load<Sprite>("img/letters/L"));
         letters.Add(Resources.Load<Sprite>("img/letters/A"));
+        letterstring.Add(KeyCode.A);
+        letters.Add(Resources.Load<Sprite>("img/letters/B"));
+        letterstring.Add(KeyCode.B);
+        letters.Add(Resources.Load<Sprite>("img/letters/C"));
+        letterstring.Add(KeyCode.C);
+        letters.Add(Resources.Load<Sprite>("img/letters/D"));
+        letterstring.Add(KeyCode.D);
+        letters.Add(Resources.Load<Sprite>("img/letters/E"));
+        letterstring.Add(KeyCode.E);
+        letters.Add(Resources.Load<Sprite>("img/letters/F"));
+        letterstring.Add(KeyCode.F);
+        letters.Add(Resources.Load<Sprite>("img/letters/G"));
+        letterstring.Add(KeyCode.G);
+        letters.Add(Resources.Load<Sprite>("img/letters/H"));
+        letterstring.Add(KeyCode.H);
+        letters.Add(Resources.Load<Sprite>("img/letters/I"));
+        letterstring.Add(KeyCode.I);
+        letters.Add(Resources.Load<Sprite>("img/letters/J"));
+        letterstring.Add(KeyCode.J);
+        letters.Add(Resources.Load<Sprite>("img/letters/K"));
+        letterstring.Add(KeyCode.K);
+        letters.Add(Resources.Load<Sprite>("img/letters/L"));
+        letterstring.Add(KeyCode.L);
+        letters.Add(Resources.Load<Sprite>("img/letters/M"));
+        letterstring.Add(KeyCode.M);
+        letters.Add(Resources.Load<Sprite>("img/letters/N"));
+        letterstring.Add(KeyCode.N);
+        letters.Add(Resources.Load<Sprite>("img/letters/O"));
+        letterstring.Add(KeyCode.O);
+        letters.Add(Resources.Load<Sprite>("img/letters/P"));
+        letterstring.Add(KeyCode.P);
+        letters.Add(Resources.Load<Sprite>("img/letters/Q"));
+        letterstring.Add(KeyCode.Q);
+        letters.Add(Resources.Load<Sprite>("img/letters/R"));
+        letterstring.Add(KeyCode.R);
+        letters.Add(Resources.Load<Sprite>("img/letters/S"));
+        letterstring.Add(KeyCode.S);
+        letters.Add(Resources.Load<Sprite>("img/letters/T"));
+        letterstring.Add(KeyCode.T);
+        letters.Add(Resources.Load<Sprite>("img/letters/U"));
+        letterstring.Add(KeyCode.U);
+        letters.Add(Resources.Load<Sprite>("img/letters/V"));
+        letterstring.Add(KeyCode.V);
+        letters.Add(Resources.Load<Sprite>("img/letters/W"));
+        letterstring.Add(KeyCode.W);
+        letters.Add(Resources.Load<Sprite>("img/letters/X"));
+        letterstring.Add(KeyCode.X);
         letters.Add(Resources.Load<Sprite>("img/letters/Y"));
+        letterstring.Add(KeyCode.Y);
         letters.Add(Resources.Load<Sprite>("img/letters/Z"));
+        letterstring.Add(KeyCode.Z);
+        wordstring.Add("PLAY");
+        wordstring.Add("OPTIONS");
+        wordstring.Add("EXIT");
     }
 
-    void Update() {
+    void fade_in_out_in()
+    {
         //Keep track of the time that has passed
-        deltaTime += Time.deltaTime; //- lastdelta;/// HHHEERRREEEEE time keeps growing. delta only needs to add
-        lastdelta = Time.deltaTime;
+        deltaTime += Time.deltaTime; // deleta varies between 0 and frameseconds.
         float slice = 0.05f;
-        //Debug.Log("deltaTime " + +deltaTime + " lastdelta " + frameSeconds);
-        if (deltaTime >= frameSeconds) {
+
+        if (deltaTime >= frameSeconds) // hit frameseconds => goto next frame
+        {
             frame += reverser;
-             frameSeconds += slice;
-             if (frame >= sprites.Count)
-             {
-                 frame = sprites.Count - 2;
-                 reverser = -1;
-             }
-             if (frame < 0)
-             {
-             frame  = 1;
-             reverser = 1;
-             }
+            frameSeconds += slice;
+            if (frame >= sprites.Count)
+            {
+                frame = sprites.Count - 2; // maximum sprite => reverse order (--)
+                reverser = -1;
+            }
+            if (frame < 0)
+            {
+                frame = 1;
+                reverser = 1; // minimum sprite => reverse order (++)
+            }
         }
         else
         {
-           // Debug.Log("bullshit : " + (deltaTime > frameSeconds) + " deltaTime " + +deltaTime + " lastdelta " + frameSeconds);
+           // keeps building up time
 
         }
         spr.sprite = sprites[frame];
-        if (Input.anyKeyDown)
+    }
+
+    void typing()
+    {
+        foreach(KeyCode input in letterstring)
         {
-            if (Input.GetKeyDown(KeyCode.P))
+            if (Input.GetKeyDown(input))
             {
-                Debug.Log("hit p");
-                word += "P";
-                GameObject letter = new GameObject("P");
+                GameObject letter = new GameObject("");
                 letter.GetComponent<Transform>().position = gameObject.GetComponent<Transform>().position;
+                letter.GetComponent<Transform>().localScale = new Vector3(0.5f, 0.5f, 0);
                 letter.AddComponent<SpriteRenderer>();
                 SpriteRenderer newletter = letter.GetComponent<SpriteRenderer>();
-               newletter.sprite = letters[1];
-                sprite_word.Add(letter);
-                gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count,0,0);
+                word += input.ToString();
+                letter.name = input.ToString();
+                newletter.sprite = letters[Convert.ToInt32((char)input) - 97];
                 
-            }
-            if (Input.GetKeyDown(KeyCode.L))
-            {
-                word += "L";
-                GameObject letter = new GameObject("L");
-                letter.GetComponent<Transform>().position = gameObject.GetComponent<Transform>().position;
-                letter.AddComponent<SpriteRenderer>();
-                SpriteRenderer newletter = letter.GetComponent<SpriteRenderer>();
-                newletter.sprite = letters[2];
                 sprite_word.Add(letter);
-                gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count, 0, 0);
-            }
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                word += "A";
-                GameObject letter = new GameObject("A");
-                letter.GetComponent<Transform>().position = gameObject.GetComponent<Transform>().position;
-                letter.AddComponent<SpriteRenderer>();
-                SpriteRenderer newletter = letter.GetComponent<SpriteRenderer>();
-                newletter.sprite = letters[3];
-                sprite_word.Add(letter);
-                gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count, 0, 0);
-
-            }
-            if (Input.GetKeyDown(KeyCode.Y))
-            {
-                word += "Y";
-                GameObject letter = new GameObject("Y");
-                letter.GetComponent<Transform>().position = gameObject.GetComponent<Transform>().position;
-                letter.AddComponent<SpriteRenderer>();
-                SpriteRenderer newletter = letter.GetComponent<SpriteRenderer>();
-                newletter.sprite = letters[4];
-                sprite_word.Add(letter);
-                gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count, 0, 0);
-
-            }
-
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                word += "Z";
-                GameObject letter = new GameObject("Z");
-                letter.GetComponent<Transform>().position = gameObject.GetComponent<Transform>().position;
-                letter.AddComponent<SpriteRenderer>();
-                SpriteRenderer newletter = letter.GetComponent<SpriteRenderer>();
-                newletter.sprite = letters[0];
-                sprite_word.Add(letter);
-                gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count, 0, 0);
-
-            }
-            //           GameObject.Find("PlayButton").GetComponent<Image>().enabled = true;
-            if (string.Compare(word,"PLAY") == 0)
-            {
-                GameObject.Find("PlayButton").GetComponent<Image>().enabled = true;
+                gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count * 0.5f, 0, 0);
             }
         }
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            if (word.Length > 0)
+            {
+                word = word.Substring(0, word.Length - 1);
+                Destroy(sprite_word[sprite_word.Count - 1]);
+                sprite_word.RemoveAt(sprite_word.Count - 1);
+                gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count * 0.5f, 0, 0);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            word += " ";
+            GameObject letter = new GameObject("Space");
+            sprite_word.Add(letter);
+            gameObject.GetComponent<Transform>().position = new Vector3(sprite_word.Count * 0.5f, 0, 0);
+        }
 
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (word.Length > 0)
+            {
+                for (int i = sprite_word.Count - 1; i >= 0; i--)
+                {
+                    Destroy(sprite_word[i]);
+                }
+                sprite_word = new List<GameObject>();
+                foreach (String action in wordstring)
+                {
+                    if (string.Compare(word, action) == 0)
+                    {
+                        // possible action
+                        DetectedWord(action);
+                    }
+                    else
+                    {
+                        // unrecognized word
+                    }
+                }
+                word = "";
+                gameObject.GetComponent<Transform>().position = new Vector3(0, 0, 0);
+            }
+        }
     }
+
+    void DetectedWord(string w)
+    {
+        Debug.Log("Found : " + w);
+        switch(w)
+        {
+            case "PLAY":
+                GameObject.Find("PlayButton").GetComponent<Image>().enabled = true;
+                GameObject.Find("OptionsButton").GetComponent<Image>().enabled = false;
+                break;
+            case "OPTIONS":
+                GameObject.Find("PlayButton").GetComponent<Image>().enabled = false;
+                GameObject.Find("OptionsButton").GetComponent<Image>().enabled = true;
+                break;
+            case "EXIT":
+                Application.Quit();
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    void Update() {
+        fade_in_out_in();
+        typing();
+    }
+    
 }
