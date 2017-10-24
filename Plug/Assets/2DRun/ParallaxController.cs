b4be35cdb9d3ac8cc4d02e73659ae7e775d74a1e
Nyarlygames <nyarlybox@gmail.com>
2017-10-24 13:16:36 +0200

    v0.0.11 player movement, parallax, some graphs

---
+++ Plug/Assets/2DRun/ParallaxController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour {
+    
+    public GameObject[] bgs;
+    public float SpeedLarge;
+    public float SpeedMiddle;
+    public float SpeedClose;
+
+    // Use this for initialization
+    void Start () {
+        bgs = GameObject.FindGameObjectsWithTag("Parallax");
+        foreach (GameObject bg in bgs)
+        {
+            if (bg.name == "Large")
+            {
+                bg.GetComponent<Transform>().Translate(-SpeedLarge * Time.deltaTime, 0f, 0f);
+            }
+            if (bg.name == "Middle") { 
+                bg.GetComponent<Transform>().Translate(-SpeedMiddle * Time.deltaTime, 0f, 0f);
+            }
+            if (bg.name == "Close")
+            {
+                bg.GetComponent<Transform>().Translate(-SpeedClose * Time.deltaTime, 0f, 0f);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject bg in bgs)
+        {
+            if (bg.GetComponent<SpriteRenderer>().isVisible == true) {
+                if (bg.name == "Large")
+                {
+                    bg.GetComponent<Transform>().Translate(-SpeedLarge * Time.deltaTime, 0f, 0f);
+                }
+                if (bg.name == "Middle")
+                {
+                    bg.GetComponent<Transform>().Translate(-SpeedMiddle * Time.deltaTime, 0f, 0f);
+                }
+                if (bg.name == "Close")
+                {
+                    bg.GetComponent<Transform>().Translate(-SpeedClose * Time.deltaTime, 0f, 0f);
+                }
+            }
+        }
+    }
+}
--- Plug/Assets/2DRun/PlayerController.cs
+++ Plug/Assets/2DRun/PlayerController.cs
@@ -5,18 +5,49 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Vector3 campos = new Vector3(0,0,0);
-    public float speed = 2.0f;
-	// Use this for initialization
-	void Start () {
-          gameObject.GetComponent<Transform>().position = new Vector3(-7, GameObject.Find("Ground").GetComponent<Transform>().position.y + GameObject.Find("Ground").GetComponent<SpriteRenderer>().sprite.bounds.size.y/2 + gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.y, 0);
-      //  Debug.Log(gameObject.GetComponent<Transform>().position + " base" + GameObject.Find("Ground").GetComponent<Transform>().position.y + " sc" + gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size.y + " sp" + GameObject.Find("Ground").GetComponent<SpriteRenderer>().sprite.bounds.size.y);
-	}
+    public float speed;
+    public bool floating = false;
+    // Use this for initialization
+    void Start()
+    {
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        Camera.main.GetComponent<Transform>().position = new Vector3(gameObject.GetComponent<Transform>().position.x + 7, 0, -5);
+	void FixedUpdate ()
+    {
 
-         gameObject.GetComponent<Transform>().Translate(5f * Time.deltaTime, 0f, 0f);
+
+        /*
+         * Floating :
+         * - Input pressed : float
+         * - Input released : drop
+         */
+        /*
+         * Ground :
+         * - Input pressed : run
+         * - Input released : jump
+         */
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (floating == false)
+            {
+                // start floating
+            }
+            else
+            {
+              //  speed += 1.0f;
+            }
+        }
+
+
+
+
+        // HERE DO : MoveTO Or AddVelocity
+        // gameObject.GetComponent<Transform>().Translate(speed * Time.deltaTime, 0f, 0f);
+        Vector3 advance = new Vector3(gameObject.GetComponent<Transform>().position.x + speed, gameObject.GetComponent<Transform>().position.y, gameObject.GetComponent<Transform>().position.z);
+        gameObject.GetComponent<Transform>().Translate(transform.right * speed * Time.deltaTime);
+        Camera.main.GetComponent<Transform>().position = new Vector3(gameObject.GetComponent<Transform>().position.x + 7, 0, -5);
+        
     }
     void OnCollisionEnter2D(Collision2D coll)
     {
