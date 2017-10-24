fc635ad67607d1db4dd681c08fd4c52127a2f4f8
Nyarlygames <nyarlybox@gmail.com>
2017-10-24 17:26:24 +0200

    camspeed

--- Plug/Assets/2DRun/PlayerController.cs
+++ Plug/Assets/2DRun/PlayerController.cs
@@ -6,16 +6,21 @@ public class PlayerController : MonoBehaviour {
 
     private Vector3 campos = new Vector3(0,0,0);
     public float speed = 3.0f;
-    public float acceleration_speed = 3.0f;
     public float max_speed = 8.0f;
+    public float acceleration_speed = 3.0f;
+    public float camspeed = 3.0f;
+    public float max_camspeed = 8.0f;
+    public float acceleration_camspeed = 3.0f;
     public int jumpheight = 300;
     public float floating_force = 2.0f;
     private float init_speed = 3.0f;
+    private float init_camspeed = 3.0f;
     public bool floating = false;
     // Use this for initialization
     void Start()
     {
         init_speed = speed;
+        init_camspeed = speed;
     }
 	
 	// Update is called once per frame
@@ -45,9 +50,22 @@ public class PlayerController : MonoBehaviour {
                 {
                     speed += acceleration_speed * Time.deltaTime;
                 }
+                else
+                {
+                    speed = max_speed;
+                }
+
+                if (camspeed < max_camspeed)
+                {
+                    camspeed += acceleration_camspeed * Time.deltaTime;
+                }
+                else
+                {
+                    camspeed = max_camspeed;
+                }
             }
         }
-        if (Input.GetKey(KeyCode.F))
+        else if (Input.GetKey(KeyCode.F))
         {
             if (floating == true)
             {
@@ -59,21 +77,38 @@ public class PlayerController : MonoBehaviour {
                 {
                     speed += acceleration_speed * Time.deltaTime;
                 }
+                else
+                {
+                    speed = max_speed;
+                }
+
+                if (camspeed < max_camspeed)
+                {
+                    camspeed += acceleration_camspeed * Time.deltaTime;
+                }
+                else
+                {
+                    camspeed = max_camspeed;
+                }
             }
 
         }
-        if (Input.GetKeyUp(KeyCode.F))
+        else if (Input.GetKeyUp(KeyCode.F))
         {
             if (floating == true)
             {
+                Debug.Log("endfloating");
+                //gameObject.GetComponent<Rigidbody>().AddForce(0, -jumpheight - 50f, 0); // need to add speed factor
+                // gameObject.GetComponent<Rigidbody>().AddForce(0, -jumpheight, 0);
                 // start droping
             }
             else
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(0, jumpheight + ((speed - 3) * 50) , 0); // need to add speed factor
-                floating = true;
                 // start jumping
                 speed = init_speed; // remove acceleration ? more like reduce it
+                camspeed = init_camspeed; // remove acceleration ? more like reduce it
+                floating = true;
             }
 
         }
@@ -85,13 +120,15 @@ public class PlayerController : MonoBehaviour {
         // HERE DO : MoveTO Or AddVelocity
         // gameObject.GetComponent<Transform>().Translate(speed * Time.deltaTime, 0f, 0f);
         gameObject.GetComponent<Transform>().Translate(transform.right * speed * Time.deltaTime);
-        Camera.main.GetComponent<Transform>().Translate(transform.right * init_speed * Time.deltaTime);
+        Camera.main.GetComponent<Transform>().Translate(transform.right * camspeed * Time.deltaTime);
 
     }
     void OnCollisionEnter(Collision coll)
     {
-        floating = false;
-        Debug.Log(coll.gameObject.tag);
+        if (coll.gameObject.tag == "Ground")
+        {
+            floating = false;
+        }
 
     }
 }
