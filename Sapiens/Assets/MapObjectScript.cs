c06bf1ae24518dd55d19764e9d46f46486a5481a
Nyarlygames <nyarlybox@gmail.com>
2018-01-15 11:17:30 +0100

    Commit map first setup

---
+++ Sapiens/Assets/MapObjectScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapObjectScript : MonoBehaviour {
+
+    public float height = 0;
+    public string Archetype = "";
+    public string RegionName = "";
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
