9f9b49fc4eb665742aad005a1126918e9f0f25e1
Nyarlygames <nyarlybox@gmail.com>
2018-02-19 13:17:09 +0100

    fire particles

--- Sapiens/Assets/Scripts/TribeMembersGO.cs
+++ Sapiens/Assets/Scripts/TribeMembersGO.cs
@@ -8,15 +8,45 @@ public class TribeMembersGO : MonoBehaviour {
     public GameObject fire;
     public Sprite[] firesprites;
     int firespritespeed = 5;
-	// Use this for initialization
-	void Start () {
-	}
+    int init = 0;
+    ParticleSystem ps;
+    // Use this for initialization
+    void Start ()
+    {
+    }
 	
 	// Update is called once per frame
 	void Update () {
        if (firesprites != null)
         {
+            if (init == 0)
+            {
+                GameObject FireParticles = new GameObject("FireParticles");
+                ps = FireParticles.AddComponent<ParticleSystem>();
+
+                ParticleSystemRenderer psr = FireParticles.GetComponent<ParticleSystemRenderer>();
+                psr.material = Resources.Load<Material>("Play/Materials/smoke_mat");
+                psr.trailMaterial = Resources.Load<Material>("Play/Materials/smoke_mat");
+                ParticleSystem.MainModule main = ps.main;
+                //main.startColor = new ParticleSystem.MinMaxGradient(Color.red, Color.yellow);
+                main.startSize = 0.05f;
+                main.startSpeed = 0.1f;
+                main.startSizeY = 0.5f;
+                main.startLifetime = 2.0f;
+                ParticleSystem.ShapeModule pss = ps.shape;
+                pss.rotation = Vector3.up;
+                pss.scale = new Vector3(0.5f, 1.0f, 1.0f);
+                pss.arc = 10.0f;
+                pss.radius = 1.0f;
+                pss.angle = 0.0f;
+                init = 1;
+                FireParticles.GetComponent<Transform>().position = fire.GetComponent<Transform>().position;
+                FireParticles.GetComponent<Transform>().SetParent(fire.GetComponent<Transform>());
+                FireParticles.GetComponent<Transform>().Rotate(new Vector3(0.0f, 0.0f, 90.0f));
+            }
             fire.GetComponent<SpriteRenderer>().sprite = firesprites[(int) (Time.time * firespritespeed % firesprites.Length)];
         }
+        //psr.material = Resources.Load<Material>("Play/Materials/smoke_mat");
+        ps.Emit(2);
     }
 }
