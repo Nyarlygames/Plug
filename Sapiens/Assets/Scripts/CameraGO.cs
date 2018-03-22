6b2f13ee0c6028965e8998bf0461256ac35eb76b
Nyarlygames <nyarlybox@gmail.com>
2018-03-22 23:34:14 +0100

    cam lock

--- Sapiens/Assets/Scripts/CameraGO.cs
+++ Sapiens/Assets/Scripts/CameraGO.cs
@@ -7,22 +7,63 @@ public class CameraGO : MonoBehaviour {
     GameManager GM;
     Transform tribePos;
     Transform camPos;
-	// Use this for initialization
-	void Start () {
+    Camera thiscam;
+    GameObject tribemem;
+
+    void Start () {
         GM = GameObject.Find("GameManager").GetComponent<GameManager>();
         camPos = gameObject.GetComponent<Transform>();
+        thiscam = gameObject.GetComponent<Camera>();
+        tribemem = GameObject.Find("Tribe_Members");
     }
 	
-	// Update is called once per frame
 	void Update ()
     {
-        if (GameObject.Find("Tribe_Members") != null)
-            tribePos = GameObject.Find("Tribe_Members").GetComponent<Transform>();
-        Vector3 temp = new Vector3(tribePos.position.x, tribePos.position.y, -gameObject.GetComponent<Camera>().orthographicSize);
+        Vector3 temp = Vector3.zero;
+        if (tribemem == null) {
+            tribemem = GameObject.Find("Tribe_Members");
+            tribePos = tribemem.GetComponent<Transform>();
+        }
+        if (((tribePos.position.x < GM.map.tilesizex /2 - thiscam.orthographicSize*2) && (tribePos.position.x > thiscam.orthographicSize*2))
+            && ((tribePos.position.y < GM.map.tilesizey /2 - thiscam.orthographicSize) && (tribePos.position.y > thiscam.orthographicSize))) {
+            temp = new Vector3(tribePos.position.x, tribePos.position.y, -thiscam.orthographicSize);
+        }
+        else if (tribePos.position.x >= GM.map.tilesizex / 2 - thiscam.orthographicSize * 2)
+        {
+            if (tribePos.position.y >= GM.map.tilesizey/2 - thiscam.orthographicSize)
+                temp = new Vector3(GM.map.tilesizex / 2 - thiscam.orthographicSize * 2, GM.map.tilesizey / 2 - thiscam.orthographicSize, -thiscam.orthographicSize);
+            else if (tribePos.position.y <= thiscam.orthographicSize)
+                temp = new Vector3(GM.map.tilesizex / 2 - thiscam.orthographicSize * 2, thiscam.orthographicSize, -thiscam.orthographicSize);
+            else
+                temp = new Vector3(GM.map.tilesizex / 2 - thiscam.orthographicSize * 2, tribePos.position.y, -thiscam.orthographicSize);
+        }
+        else if (tribePos.position.x <= thiscam.orthographicSize * 2)
+        {
+            if (tribePos.position.y >= GM.map.tilesizey / 2 - thiscam.orthographicSize)
+                temp = new Vector3(thiscam.orthographicSize * 2, GM.map.tilesizey / 2 - thiscam.orthographicSize, -thiscam.orthographicSize);
+            else if (tribePos.position.y <= thiscam.orthographicSize)
+                temp = new Vector3(thiscam.orthographicSize * 2, thiscam.orthographicSize, -thiscam.orthographicSize);
+            else
+                temp = new Vector3(thiscam.orthographicSize * 2, tribePos.position.y, -thiscam.orthographicSize);
+        }
+        else if (tribePos.position.y >= GM.map.tilesizey / 2 - thiscam.orthographicSize)
+        {
+            if (tribePos.position.x >= GM.map.tilesizex / 2 - thiscam.orthographicSize * 2)
+                temp = new Vector3(GM.map.tilesizex / 2 - thiscam.orthographicSize * 2, GM.map.tilesizey / 2 - thiscam.orthographicSize, -thiscam.orthographicSize);
+            else if (tribePos.position.x <= thiscam.orthographicSize * 2)
+                temp = new Vector3(thiscam.orthographicSize * 2, GM.map.tilesizey / 2 - thiscam.orthographicSize, -thiscam.orthographicSize);
+            else
+                temp = new Vector3(tribePos.position.x, GM.map.tilesizey / 2 - thiscam.orthographicSize, -thiscam.orthographicSize);           
+        }
+        else if (tribePos.position.y <= thiscam.orthographicSize)
+        {
+            if (tribePos.position.x >= GM.map.tilesizex / 2 - thiscam.orthographicSize * 2)
+                temp = new Vector3(GM.map.tilesizex / 2 - thiscam.orthographicSize * 2, thiscam.orthographicSize, -thiscam.orthographicSize);
+            else if (tribePos.position.x <= thiscam.orthographicSize * 2)
+                temp = new Vector3(thiscam.orthographicSize * 2, thiscam.orthographicSize, -thiscam.orthographicSize);
+            else
+                temp = new Vector3(tribePos.position.x, thiscam.orthographicSize, -thiscam.orthographicSize);
+        }
         camPos.position = temp;
-       /* tribePos = GM.TribeGO.GetComponent<TribeGO>().CharsGO[0].GetComponent<Transform>();
-        Vector3 temp = tribePos.position;
-        temp.z -= 5.0f;
-        camPos.position = temp;*/
-	}
+    }
 }
